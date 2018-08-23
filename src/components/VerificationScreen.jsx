a9cd1f3bfc5192e63cf6169252923b1686593697
LiyaGaneyeva <liya.ganeeva@flatstack.com>
2018-08-23 19:20:23 +0300

    add form and edit logo

--- src/components/VerificationScreen.jsx
+++ src/components/VerificationScreen.jsx
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Button from './Button';
-import { QUESTION_TOTAL } from '../constants';
 
 const StyledButton = styled(Button)`
   width: 200px;
@@ -15,17 +14,16 @@ const InfoText = styled.p`
 `;
 
 const VerificationScreen = ({ counter, reset }) => {
-  const percentCounter = (counter / QUESTION_TOTAL) * 50;
   return (
     <Fragment>
-      {percentCounter <= 30 && (
+      {counter >= 40 && (
         <Fragment>
           <h1>Волноваться не стоит</h1>
           <InfoText>Никаких видимых признаков рака у вас нет. Опасаться, причин нет.</InfoText>
         </Fragment>
       )}
-      {percentCounter >= 30 &&
-        percentCounter < 60 && (
+      {counter >= 30 &&
+        counter < 40 && (
           <Fragment>
             <h1>Подумайте о визите к специалисту</h1>
             <InfoText>
@@ -36,9 +34,9 @@ const VerificationScreen = ({ counter, reset }) => {
             </InfoText>
           </Fragment>
         )}
-      {percentCounter > 60 && (
+      {counter < 30 && (
         <Fragment>
-          <h1>Стоит обратиться к врачу</h1>
+          <h1>Вам следует не откладывать визит к специалисту. </h1>
           <InfoText>
             Многие симптомы являются прямыми признаками наличия злокачественной опухоли в организме.<br />
             Но, не забывайте, что подобные проявления могут быть проявлениями и других заболеваний.<br />
